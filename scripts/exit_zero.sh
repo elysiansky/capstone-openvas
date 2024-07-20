7c627f8bfa96bfdcbf39c67c40bd20029a6bcc77
NadaKamel03 <nadakamel2003@gmail.com>
2024-07-20 16:01:41 -0400

    feat(edit): script change

--- scripts/exit_zero.sh
+++ scripts/exit_zero.sh
@@ -1,37 +1,7 @@
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: openvas-notus-deployment
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: openvas-notus
-  template:
-    metadata:
-      labels:
-        app: openvas-notus
-    spec:
-      containers:
-      - name: openvas-notus-container
-        image: elysiansky/capstone-openvas-notus-data:latest
-        ports:
-        - containerPort: 80
-        readinessProbe:
-          exec:
-            command:
-            - /bin/sh
-            - -c
-            - |
-              /scripts/check_health.sh
-          initialDelaySeconds: 10
-          periodSeconds: 30
-        livenessProbe:
-          exec:
-            command:
-            - /bin/sh
-            - -c
-            - |
-              /scripts/check_health.sh
-          initialDelaySeconds: 10
-          periodSeconds: 30
+#!/bin/bash
+
+if [[ "$xxxx" == "n" ]]; then
+  exit 0
+else
+  exit 1
+fi
